ed8597df537de92a6b91cf9da85281ac1155f2e9
Derrick Lyndon Pallas <derrick@pallas.us>
2021-02-16 18:31:14 -0800

    v4.0.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = 'llhttp',
-    version = '3.0.0.1',
+    version = '4.0.0.0',
     description = ("llhttp in python"),
     url = "http://github.com/pallas/pyllhttp",
     author = "Derrick Lyndon Pallas",
